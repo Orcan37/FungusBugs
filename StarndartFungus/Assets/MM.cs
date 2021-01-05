97aad88099ac8a582093c679c388a439acd1abbe
Андрей <>
2021-01-05 21:53:03 +0300

    Bug No save Vecor2 and prefab localization

---
+++ StarndartFungus/Assets/MM.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MM : MonoBehaviour
+{ 
+    void Start()
+    {
+        GameObject prefab = Resources.Load<GameObject>("TestResourcesFlowchart");
+        GameObject go = Instantiate(prefab) as GameObject; 
+        go.name = "TestResourcesFlowchart";
+    }
+ 
+}
